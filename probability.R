ae961dd5f72a1334ddb604ac0a5c042c6676dea1
rafaelsommer1 <rafaelcasommer@gmail.com>
2020-05-06 00:35:17 -0300

    commit

---
+++ probability.R
@@ -0,0 +1,7 @@
+# Probability with R
+# Author: Rafael Sommer
+# This script provide information and R scripts that I used to study probability
+# Also, some of the scripts may be helpful when dealing with bayesian statistics and generating data
+
+# Binomial distribution
+
